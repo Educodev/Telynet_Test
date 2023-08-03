10266bf05788775b96907a40a716b1e8ba217f32
Eduardo Herrera <eduardoherrera@educodev.com>
2023-08-03 16:36:52 +0200

    Initial Commit

---
+++ Telynet_Test/Telynet_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telynet_TestApp.swift
+//  Telynet_Test
+//
+//  Created by Eduardo Herrera on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Telynet_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
